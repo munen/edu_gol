99d6cbe87fc25ae413e6fb6e80870b6fe28e7096
preek <alain.lafon@dispatched.ch>
2012-01-07 15:13:57 +0100

    add main mock

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+#include "optparse.h"
+
+int height, width = 0;
+char *path        = NULL;
+
+int main(int argc, char *argv[]) {
+  if(!optparse(argc, argv))
+        abort();
+
+  return 0;
+}
