3c867e68ba7e4ce59b661e93d80a53ad5f52ee85
preek <alain.lafon@dispatched.ch>
2012-01-28 00:40:10 +0100

    add structures for world and cells

--- main.c
+++ main.c
@@ -5,6 +5,15 @@
 int height, width = 0;
 char *path        = NULL;
 
+typedef struct cell_t_ {
+  _Bool alive;
+  _Bool alive_next_round;
+} cell_t;
+
+typedef struct world_t_ {
+  cell_t **cells;
+} world_t;
+
 int main(int argc, char *argv[]) {
   if(!optparse(argc, argv))
         abort();
