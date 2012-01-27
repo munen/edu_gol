bc2dc62b6a8604315bd65bf21ea179604fb25aeb
preek <alain.lafon@dispatched.ch>
2012-01-28 00:48:52 +0100

    dynamically allocate memory for a world of given height and width

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "optparse.h"
 
@@ -14,8 +15,33 @@ typedef struct world_t_ {
   cell_t **cells;
 } world_t;
 
+
+_Bool create_world(world_t *world) {
+  int i;
+
+  // allocate memory to hold all lines in the world
+  world->cells = (cell_t **) malloc(sizeof (cell_t *) * height);
+
+  // for each line in the world, assign memory to it
+  for(i = 0; i<=height; i++) {
+    if(!(world->cells[i] = (cell_t *) malloc(sizeof(cell_t *) * width)))
+      return false;
+  }
+
+  return true;
+}
+
+
 int main(int argc, char *argv[]) {
   if(!optparse(argc, argv))
         abort();
+
+  world_t world;
+
+  if(!create_world(&world))
+    printf("error creating world\n");
+  else
+    printf("created world\n");
+  
   return 0;
 }
